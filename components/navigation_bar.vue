90b8d95a1ef4ce25d2004cf9d63b9e264854b16f
varangian111 <rrowe222@gmail.com>
2019-09-10 23:15:39 -0500

    noticing a bludge

---
+++ components/navigation_bar.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <nuxt-link to="/index">Home</nuxt-link>
+    <nuxt-link to="/index">Gallery</nuxt-link>
+  </div>
+</template>
+
+<script>
+	export default {
+		name: "navigation_bar"
+	}
+</script>
+
+<style scoped>
+
+</style>
---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+/*
+** TailwindCSS Configuration File
+**
+** Docs: https://tailwindcss.com/docs/configuration
+** Default: https://github.com/tailwindcss/tailwindcss/blob/master/stubs/defaultConfig.stub.js
+*/
+module.exports = {
+  theme: {
+    extend : {
+      colors : {
+        'Pornhub-black' : '#1b1b1b',
+        'Pornhub-dark-gray' : '#292929',
+        'Pornhub-light-grey' : '#808080',
+        'Pornhub-orange' : '#ffa31a'
+      }
+    }
+  },
+  variants: {},
+  plugins: []
+}
