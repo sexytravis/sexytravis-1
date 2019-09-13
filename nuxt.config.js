f02c218a4f2b723f27a1b2505d0cf504a13ef871
varangian111 <rrowe222@gmail.com>
2019-09-13 12:55:38 -0500

    Package name

--- nuxt.config.js
+++ nuxt.config.js
@@ -5,11 +5,12 @@ export default {
 	** Headers of the page
 	*/
 	head: {
-		title: process.env.npm_package_name || '',
+		title: "UwU, notices your bulge" || '',
 		meta: [
 			{ charset: 'utf-8' },
 			{ name: 'viewport', content: 'width=device-width, initial-scale=1' },
-			{ hid: 'description', name: 'description', content: process.env.npm_package_description || '' }
+			{ hid: 'description', name: 'UwU, notices your bulge', content: process.env.npm_package_description || '' },
+
 		],
 		link: [
 			{ rel: 'icon', type: 'image/x-icon', href: '/icon.png' }
